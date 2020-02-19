a7259628f321c6a01068534761275a5375993f2a
Praveen Paul <praveen.paul@Praveenpaul.local>
2020-02-20 02:53:39 +0530

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/org/nathan/jf/build/prop/editor/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.nathan.jf.build.prop.editor;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.nathan.jf.build.prop.editor";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "2.0.1";
+}
